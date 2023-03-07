75334c49356dc4374c0976dcc513e26a1778d9b2
David Olagookun <erijesudo@gmail.com>
2023-03-07 11:03:59 +0100

    Initial Commit

---
+++ trekker/trekkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trekkerApp.swift
+//  trekker
+//
+//  Created by David OH on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct trekkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
